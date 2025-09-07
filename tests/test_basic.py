a720a0adcd637c1a135da2fc9de23de0359f0240
dhdhxji <keysindie@gmail.com>
2025-09-07 13:02:44 +0000

    Add pyproject.toml

---
+++ tests/test_basic.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestJinja2Toolbox(unittest.TestCase):
+    def test_basic(self):
+        # TODO: Replace with real tests
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
